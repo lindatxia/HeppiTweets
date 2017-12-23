5c90521031bf10e11445c550c66588f2c4c0b309
Linda Xia <lindax@andrew.cmu.edu>
2017-12-23 00:31:31 -0800

    Added authentication details

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import twitter
 import os
+import json
+with open('config.json') as data: 
+	data = json.load(data)
+
+
+consumer_key = data["consumer_key"]
+consumer_secret = data["consumer_secret"]
+access_token = data["access_token"]
+access_secret = data["access_secret"]
+
+# Log in to Twitter
+tweetbot.twitter_login(consumer_key, consumer_secret, access_token, access_secret)

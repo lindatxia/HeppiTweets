5cd3f58d051df1244dcc16dd36ac1cfffae0a76d
Linda Xia <lindax@andrew.cmu.edu>
2017-12-23 20:23:15 -0800

    Implemented basic functionality, without automated postings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import twitter
-import os
+from twitter import *
+from TwitterSearch import *
 import json
+import time
 with open('config.json') as data: 
 	data = json.load(data)
 
@@ -10,5 +11,47 @@ consumer_secret = data["consumer_secret"]
 access_token = data["access_token"]
 access_secret = data["access_secret"]
 
-# Log in to Twitter
-tweetbot.twitter_login(consumer_key, consumer_secret, access_token, access_secret)
+# Connects to the Twitter Search Python Wrapper API
+def getTweetList():
+	tweetList = []
+	try:
+		tso = TwitterSearchOrder() 
+		tso.set_keywords(['Blessed', 'Happy', 'Surprise', 'Believe', 'Positive', 'Celebrate', 'Engaged', 'Offer', 'Holidays', 'Beautiful', 'Nature', 'Love'],or_operator = True) # let's define all words we would like to have a look for
+		tso.set_language('en') 
+		tso.set_include_entities(False) 
+		tso.set_positive_attitude_filter()
+		tso.remove_link_filter()
+
+		ts = TwitterSearch(
+			consumer_key = data["consumer_key"],
+			consumer_secret = data["consumer_secret"],
+			access_token = data["access_token"],
+			access_token_secret = data["access_secret"]
+		)
+
+		def my_callback_closure(current_ts_instance): # accepts ONE argument: an instance of TwitterSearch
+			queries, tweets_seen = current_ts_instance.get_statistics()
+			if queries > 0 and (queries % 5) == 0: # trigger delay every 5th query
+				time.sleep(18000)
+
+		for tweet in ts.search_tweets_iterable(tso, callback=my_callback_closure):
+			print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+			tweetList.append(tweet)
+
+	except TwitterSearchException as e: 
+		print(e)
+
+	return tweetList
+
+
+# Connects to the Twitter Python Wrapper API
+def connect():
+    return Twitter(auth=OAuth(access_token, access_secret, consumer_key, consumer_secret))
+
+
+# Call this every 5 minutes
+api = connect()
+# api.statuses.update(status=)
+
+tw = getTweetList()
+
